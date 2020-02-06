2e44c10daa675ae75e318671856a8984e07928d2
chamseddinekh <chamsseddine.khedhri@gmail.com>
2020-02-06 16:52:34 +0000

    putchar

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - entry block
+ * @void: no argument
+ * Return: 0 (Sucess)
+ **/
+int main(void)
+
+for (int x = 'A'; x <= 'Z'; x++)
+{
+putchar(tolower(x));
+}
+return (0);
+}
