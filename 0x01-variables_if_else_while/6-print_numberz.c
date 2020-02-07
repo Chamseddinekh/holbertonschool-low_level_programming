478d7e7e2e85130952f1fe83d398acf9ff1ab15d
chamseddinekh <chamsseddine.khedhri@gmail.com>
2020-02-07 12:56:36 +0000

    number

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+/**
+ * main - entry block
+ * @void: no argument
+ * Return: 0 (Sucess)
+ **/
+int main(void)
+{
+int i = 0;
+for (i = 0; i < 10; i++)
+{
+putchar(i + '0');
+}
+ putchar('\n');
+return (0);
+}
