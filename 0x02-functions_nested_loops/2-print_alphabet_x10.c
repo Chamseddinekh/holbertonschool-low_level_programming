c9ad433aa1c4ab5a53c7729b28ba2618abaed786
chamseddinekh <chamsseddine.khedhri@gmail.com>
2020-02-10 22:57:47 +0000

    10time

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+/**
+ *print_alphabet_x10- use only user defined variables to create output
+ *
+ *Return: print to stdout the alphabet, in lowercase, 10 times with new line .
+ **/
+void print_alphabet_x10(void)
+{
+char i;
+int j;
+for (j = 0; j < 10; j++)
+{
+for (i = 'a'; i <= 'z'; i++)
+{
+_putchar(i);
+}
+_putchar('\n');
+}
+_putchar('\n');
+
+}
