5d5b404e4c810b3aeac11602a849fcdd7583c0ae
chamseddinekh <chamsseddine.khedhri@gmail.com>
2020-02-10 22:42:42 +0000

    Alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,11 +1,15 @@
 #include "holberton.h"
+/**
+ *print_alphabet- use only user defined variables to create output
+ *
+ *Return: print to stdout the alphabet, in lowercase, followed by a new line.
+ **/
 void print_alphabet(void)
 {
-int i;
-for (i = 'a';i < 'z'; i++)
+char i;
+for (i = 'a'; i <= 'z'; i++)
 {
- _putchar(i);
+_putchar(i);
 }
 _putchar('\n');
-return (0);
 }
