018b80049d2d853099132a2288ad552289c47ef5
chamseddinekh <chamsseddine.khedhri@gmail.com>
2020-02-11 21:13:53 +0000

    a

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+/**
+ *print_to_98- use only user defined variables to create output
+ *@n : variable input
+ *Return: 0 if the character is lower 1 fi not
+ **/
+void print_to_98(int n)
+{
+int i;
+if (n < 98)
+{
+for (i= n; i < 98; i++)
+{
+printf("%d, ", i);
+}
+printf("98\n");
+}
+else if (n == 98)
+{
+printf("%d\n", n);
+}
+else
+{
+for (i = n; i > 98; i--)
+{
+printf("%d, ", i);
+
+}
+printf("98\n");
+}
+}
