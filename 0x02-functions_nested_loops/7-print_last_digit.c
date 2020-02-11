513181dc607fcb67dc6bd99daa56c3e29753e5f5
chamseddinekh <chamsseddine.khedhri@gmail.com>
2020-02-11 14:41:09 +0000

    lastdig

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -7,14 +7,16 @@
 int print_last_digit(int n)
 {
 int res, m;
-if (n > 0)
+if (n >= 0)
 {
 res = (n % 10);
 res = _putchar(res + '0');
 return (res);
 }
 else
+{
 m = ((n < 0) ? (-n) : (n));
 res = m % 10;
 return (res);
+}
 }
