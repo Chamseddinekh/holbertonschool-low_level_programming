c5f5f83674ca691fd28e3b882b110a848b0499f3
chamseddinekh <chamsseddine.khedhri@gmail.com>
2020-02-11 17:20:46 +0000

    a

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+/**
+ *add- use only user defined variables to create output
+ *@a : variable input
+ *@b : input variable
+ *Return: 0 if the character is lower 1 fi not
+ **/
+int add(int a, int b)
+{
+int res;
+res = a + b;
+return (res);
+}
