a66988d160117d33528f794539fb86ce19166e49
chamseddinekh <chamsseddine.khedhri@gmail.com>
2020-02-11 14:06:01 +0000

    lastdifit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+ *print_last_digit- use only user defined variables to create output
+ *@n: type of input
+ *Return: 0 if the character is lower 1 fi not
+ **/
+int print_last_digit(int n)
+{
+int res, m;
+if (n > 0)
+{
+res = (n % 10);
+return (res);
+}
+else
+m = ((n < 0) ? (-n) : (n));
+res = m % 10;
+return (res);
+}
