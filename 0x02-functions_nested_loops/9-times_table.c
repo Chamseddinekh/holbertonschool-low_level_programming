db510079011b067226a4a71bff38bf7a5fd28815
chamseddinekh <chamsseddine.khedhri@gmail.com>
2020-02-12 14:23:54 +0000

    a

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,31 @@
+#include "holberton.h"
+/**
+ *times_table - use only user defined variables to create output
+ *
+ *
+ **/
+void times_table(void)
+{
+int i, j, res;
+for (i = 0; i < 10; i++)
+{
+for (j = 0; j < 10; j++)
+{
+res = i * j;
+if (res > 9)
+{
+_putchar((res / 10) + '0');
+_putchar((res % 10) + '0');
+}
+else
+{
+_putchar(res + '0');
+}
+if (j < 9)
+_putchar(',');
+_putchar(' ');
+}
+_putchar('\n');
+
+}
+}
