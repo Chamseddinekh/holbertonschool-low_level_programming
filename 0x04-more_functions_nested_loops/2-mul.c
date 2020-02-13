9ac334f27b1419e7b64f58ba40b319572321a4b2
chamseddinekh <chamsseddine.khedhri@gmail.com>
2020-02-13 09:29:30 +0000

    mul

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+/**
+ *mul - use only user defined variables to create output
+ *@a: number input
+ *@b: number input
+ *Return: multiplies two integers
+ **/
+int mul(int a, int b)
+{
+int res;
+res = a * b;
+return (res);
+}
