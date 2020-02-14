592ba00b1dac44fa336be2bfe0ead3ff217bd029
chamseddinekh <chamsseddine.khedhri@gmail.com>
2020-02-14 13:24:26 +0000

    a

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -6,7 +6,7 @@
  **/
 void print_triangle(int size)
 {
-  int i, j, k;
+int i, j, k;
 if (size <= 0)
 {
 _putchar ('\n');
@@ -14,7 +14,7 @@ _putchar ('\n');
 else
 for (i = 0; i < size; i++)
 {
-for (j = 0; j < size - i; j++)
+for (j = 1; j < size - i; j++)
 {
 _putchar (' ');
 }
