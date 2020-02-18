be9c86c51f0d54dceed9cdfbb8854138e08f3ebb
chamseddinekh <chamsseddine.khedhri@gmail.com>
2020-02-18 17:30:01 +0000

    a

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -3,7 +3,7 @@ void puts_half(char *str)
 int i, n, len = 0;
 while (str[len] != '\0')
 len++;
-
+len = len-1
 if (len % 2 == 0)
 {
 n = len / 2;
@@ -12,7 +12,7 @@ _putchar(str[i]);
 }
 else
 {
-n = (len + 1) / 2;
+n = (len - 1) / 2;
 for (i = n; i < len; i++)
 _putchar(str[i]);
 }
