52a5d1eb315c7d3c5e91f08e3f884e7eb60401fa
chamseddinekh <chamsseddine.khedhri@gmail.com>
2020-02-18 13:30:04 +0000

    a

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,4 +1,4 @@
-#include "holberton.h"
+B#include "holberton.h"
 /**
  *  rev_string - entry block
  * @s: string
@@ -6,7 +6,7 @@
  **/
 void rev_string(char *s)
 {
-int j, i, len = 0;
+int i, len = 0;
 while (s[len] != '\0')
 len++;
 len = len - 1;
@@ -15,10 +15,11 @@ s_begin = s;
 s_end = s;
 for (i = 0; i < len; i++)
 s_end++;
-for (i = 0; i < len / 2; i++) 
+for (i = 0; i < len / 2; i++)
 {
 c = *s_end;
 *s_end = *s_begin;
 *s_begin = c;
 s_begin++;
-s_end--;}
+s_end--;
+}
