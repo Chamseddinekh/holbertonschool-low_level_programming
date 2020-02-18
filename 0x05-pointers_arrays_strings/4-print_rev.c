16478006ab40003c5a9069d3cb6468363cc97e73
chamseddinekh <chamsseddine.khedhri@gmail.com>
2020-02-18 12:58:30 +0000

    rev

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -4,16 +4,14 @@
  * @s: string
  * Return: 0 (Sucess)
  **/
+
 void print_rev(char *s)
 {
-int len = 0;
-while (s[len])
+int i, len = 0;
+while (s[len] != '\0')
 len++;
-
-while(s[len - 1])
-{
-_putchar(s[len]);
-len--;
-}
+for (i = len - 1; i >= 0; i--)
+_putchar(s[i]);
 _putchar('\n');
+
 }
