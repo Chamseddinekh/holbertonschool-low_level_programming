5ae109aaaebd8c757b4a97accf80c49e8ed15ab6
chamseddinekh <chamsseddine.khedhri@gmail.com>
2020-02-18 16:10:44 +0000

    a

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,4 +1,4 @@
-B#include "holberton.h"
+#include "holberton.h"
 #include <stdio.h>
 /**
  *  print_array - entry block
@@ -15,6 +15,6 @@ for (i = 0; i < n; i++)
 if (i < n - 1)
 printf("%d, ", a[i]);
 else
-printf("%d", a[i]);
+printf("%d\n", a[i]);
 }
 }
