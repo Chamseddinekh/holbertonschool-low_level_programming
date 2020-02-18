c8e3c1a095ba7e90d4dafffd4330a5023f4b8669
chamseddinekh <chamsseddine.khedhri@gmail.com>
2020-02-18 10:23:16 +0000

    update

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -6,13 +6,13 @@
  **/
 void puts2(char *str)
 {
-int i;
-i = 0;
-while (str[i] != '\0')
+int i,len;
+i=0;
+len =0;
+len = _strlen(str);
+for (i=0; i < len; i += 2)
 {
 _putchar(str[i]);
-i++;
-i++;
 }
 _putchar('\n');
 }
