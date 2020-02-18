1a91133c5e24f34a184b60720ed9f6069058095c
chamseddinekh <chamsseddine.khedhri@gmail.com>
2020-02-18 17:23:56 +0000

    a

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -2,15 +2,20 @@
 #include <stdio.h>
 /**
  *  print_array - entry block
- * @a: pointer
+ * @a: pointer 
  *@n : var
  * Return: 0 (Sucess)
  **/
 void print_array(int *a, int n)
 {
 int i;
+
 for (i = 0; i < n; i++)
-printf("%d, ", a[i]);
-printf("%d", a[n - 1]);
+{
+if (i < n - 1)
+printf("%d, ",a[i]);
+else
+printf("%d",a[i]);
+}
 printf("\n");
 }
