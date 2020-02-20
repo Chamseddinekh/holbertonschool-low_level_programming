e9f8538cd8cc20e3cf2fcfaeacf6616f3172bb06
chamseddinekh <chamsseddine.khedhri@gmail.com>
2020-02-20 18:57:38 +0000

    cap

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -7,13 +7,15 @@
 char *cap_string(char *str)
 {
 int symb[14] = {46, 34, 125, 32, 44, 40, 63, 33, 9, 59, 41, 123, 10};
-int i,  j;
+int i, j, t;
 for (i = 0; str[i] != '\0'; i++)
 {
 if (str[0] >= 97 && str[0] <= 122)
+{
 str1[0] = str1[0] - 32;
-for (k = 0; k < 11; k++)
-if (str[i] >= 97 && str[i] <= 122 && str[i - 1] == symb[k])
+}
+for (t = 0; t < 11; t++)
+if (str[i] >= 97 && str[i] <= 122 && str[i - 1] == symb[t])
 str[i] = str[i] - 32;
 }
 return (str);
