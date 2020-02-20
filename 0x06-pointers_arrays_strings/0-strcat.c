65841b94acce0d87e0c590b85aa5da23f4bfb478
chamseddinekh <chamsseddine.khedhri@gmail.com>
2020-02-20 13:13:23 +0000

    struct

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+/**
+ *  *_strcat - entry block
+ * @dest: pointer
+ *@src : var
+ * Return: dest
+ **/
+
+char *_strcat(char *dest, char *src)
+{
+int i, j;
+i = 0;
+j = 0;
+while (dest[i] != '\0')
+{
+i++;
+}
+while (src[j] != '\0')
+{
+dest[i] = src[j];
+i++;
+j++;
+}
+return (dest);
+}
