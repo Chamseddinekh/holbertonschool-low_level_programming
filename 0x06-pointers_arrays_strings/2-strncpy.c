522d28740cad0d9f5cf1a0dec358ecf1a1009d04
chamseddinekh <chamsseddine.khedhri@gmail.com>
2020-02-20 15:58:20 +0000

    strncpy

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,11 @@
+char *_strncpy(char *dest, char *src, int n)
+{
+int i, j;
+for (j = 0; src[j] != '\0'; j++)
+;
+for (i = 0; src[i] != '\0' && i < n; i++)
+dest[i] = src[i];
+for (; i <= n; i++)
+dest[j] = '\0';
+return (dest);
+}
