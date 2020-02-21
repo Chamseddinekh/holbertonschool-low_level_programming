a64309b671c3bf45cb75ef9e5ac771a1e59d0142
chamseddinekh <chamsseddine.khedhri@gmail.com>
2020-02-21 22:53:35 +0000

    a

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+/**
+ * *leet - check the code for Holberton School students.
+ * @string: variable
+ * Return: Always 0.
+ */
+char *leet(char *string)
+{
+int i, j, k;
+k = 0;
+char alph1 [6] = {52, 48, 51, 55, 49};
+char alph [11] = {'A', 'a', 'O', 'o', 'E', 'e', 't', 'T', 'l', 'L'};
+for (i = 0; string[i] != '\0'; i++)
+{
+for (j = 0; alph[j] != '\0'; j++)
+if (string[i] == alph[j])
+{
+string[i] = alph1[k + j / 2];
+}
+}
