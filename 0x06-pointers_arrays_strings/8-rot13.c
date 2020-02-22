79f0c6253593ca02a1c540efbdf477718b861be3
chamseddinekh <chamsseddine.khedhri@gmail.com>
2020-02-22 17:13:55 +0000

    rot13

---
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+/**
+ * *rot13 - check the code for Holberton School students.
+ * @string: string input
+ * Return: string rot13
+ */
+char *rot13(char *string)
+{
+int i, j;
+char alph [] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+char alph1 [] = {'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm'};
+for (i = 0; string[i] != '\0'; i++)
+{
+for (j = 0; alph[j] != '\0'; j++)
+if (string[i] == alph[j])
+{
+string[i] = alph1[j];
+}
+}
+return (string);
+}
+
