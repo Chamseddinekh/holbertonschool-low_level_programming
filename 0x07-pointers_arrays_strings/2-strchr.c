34a68770b57c627a0027d30f91d0921870a81867
chamseddinekh <chamsseddine.khedhri@gmail.com>
2020-02-24 15:48:45 +0000

    a

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -13,7 +13,13 @@ char *str = NULL;
 
 for (j = 0; s[j] != '\0'; j++)
 {
-  if (s[j] == c || s[j] == s[j + 1])
+if (s[j] == c || s[j] == s[j + 1])
+{
+str = &s[j];
+return (str);
+break;
+}
+else if (s[j] == c)
 {
 str = &s[j];
 return (str);
