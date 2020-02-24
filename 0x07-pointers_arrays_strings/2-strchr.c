8d5c788f18827a42ccb8a4f0c17b420b90dff69e
chamseddinekh <chamsseddine.khedhri@gmail.com>
2020-02-24 13:30:18 +0000

    chr

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,24 +1,24 @@
 #include "holberton.h"
 /**
- * *_memcpy - check the code for Holberton School students.
+ * *_strchr - check the code for Holberton School students.
  * @s: string
  * @c: positive number
  * Return: dest
  */
 char *_strchr(char *s, char c)
 {
-int j = 0, len = 0;
+int j = 0;
 char *str;
-while (len != '\0')
+
+while (s[j] != '\0')
 {
-len++;
-}
-len = len - 1;
 while (s[j] != c && s[j] != s[j + 1])
 {
 j++;
 }
+
 str = &s[j];
 return (str);
+}
 return (0);
 }
