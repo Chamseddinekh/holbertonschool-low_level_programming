f8f968a46e5d351dc26a3c566bcf9b495fedb1af
chamseddinekh <chamsseddine.khedhri@gmail.com>
2020-02-25 16:41:34 +0000

    a

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+/**
+ * *_strstr - check the code for Holberton School students.
+ * @haystack: sxsd
+ * @needle: positive number
+ * Return: str
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+while (*haystack)
+{
+char *Begin = haystack;
+char *pattern = needle;
+while (*haystack && *pattern && *haystack == *pattern)
+{
+haystack++;
+pattern++;
+}
+if (!(*pattern))
+return (Begin);
+haystack = Begin + 1;
+}
+return (NULL);
+}
