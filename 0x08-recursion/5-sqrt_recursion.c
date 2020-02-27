2f89a87b9fe5eeec493963c5f138bc7cd8cea5e3
chamseddinekh <chamsseddine.khedhri@gmail.com>
2020-02-27 14:27:10 +0000

    a

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,27 @@
+B#include "holberton.h"
+/**
+ *natural_sqrt - sqrt
+ *@num: int
+ *@i: int
+ *Return: sqrt number
+ */
+int natural_sqrt(int num, int i)
+{
+while (i * i != num && i < num)
+i++;
+if (i *i == num)
+return (i);
+else
+return (-1);
+}
+/**
+ *_sqrt_recursion - result
+ *@n: int
+ *Return: sqrt
+*/
+int _sqrt_recursion(int n)
+{
+int i;
+i = natural_sqrt(n, 0);
+return (i);
+}
