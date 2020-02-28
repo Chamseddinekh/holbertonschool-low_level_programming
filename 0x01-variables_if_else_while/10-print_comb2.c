d85672cdd942aa36dd9c19acaf00b82ca9158dd9
chamseddinekh <chamsseddine.khedhri@gmail.com>
2020-02-28 19:43:12 +0000

    combo u^date

--- 0x01-variables_if_else_while/10-print_comb2.c
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -6,27 +6,18 @@
  **/
 int main(void)
 {
-int i;
-for (i = 0; i <= 99; i++)
+int i, j;
+for (i = 0; i <= 9; i++)
 {
-if (i < 9)
+for (j = 0; j <= 9; j++)
 {
-putchar(0 + '0');
-putchar(i + '0');
+putchar('0' + i);
+putchar('0' + j);
+if (i != 9 || j != 9)
+{
 putchar(',');
 putchar(' ');
 }
-else if (i == 99)
-{
-putchar((i / 10) + '0');
-putchar((i % 10) + '0');
-}
-else
-{
-putchar((i / 10) + '0');
-putchar((i % 10) + '0');
-putchar(',');
-putchar(' ');
 }
 }
 putchar('\n');
