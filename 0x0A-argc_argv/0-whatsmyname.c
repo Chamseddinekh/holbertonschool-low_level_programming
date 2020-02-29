410a1cf34d13398e6ce6f1c1f03ba59c0a0eb5c2
chamseddinekh <chamsseddine.khedhri@gmail.com>
2020-02-29 13:57:30 +0000

    name file

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,7 +1,13 @@
 #include<stdio.h>
+/**
+ * main - printing digit
+ *@argc: number of agruments
+ *@argv: table of arguments
+ *Return: print all possible different combinations of digits
+**/
 int main(int argc, char **argv)
 {
 argc = 0;
-printf("%s\n",argv[argc]);
-return(0);
+printf("%s\n", argv[argc]);
+return (0);
 }
