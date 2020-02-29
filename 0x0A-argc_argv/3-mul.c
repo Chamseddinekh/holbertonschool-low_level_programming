59fd71ac477ea04255f1de13ee331f39f2a6628c
chamseddinekh <chamsseddine.khedhri@gmail.com>
2020-02-29 15:00:42 +0000

    mul

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<stdlib.h>
+/**
+ * main - printing digit
+ *@argc: number of agruments
+ *@argv: table of arguments
+ *Return: number of arguments
+ **/
+int main(int argc, char **argv)
+{
+int i, mul = 1;
+for (i = 0; i < argc; i++)
+;
+if (i < 2)
+{
+printf("Error\n");
+return (1);
+}
+else
+for (i = 1; i < argc; i++)
+
+mul = mul * atoi(argv[i]);
+printf("%d\n", mul);
+
+return (0);
+}
