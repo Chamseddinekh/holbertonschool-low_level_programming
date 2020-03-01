a3cba71918c38dd433987bb21511c6b0def9cd64
chamseddinekh <chamsseddine.khedhri@gmail.com>
2020-03-01 14:38:36 +0000

    a

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -9,7 +9,7 @@
  **/
 int main(int argc, char **argv)
 {
-int j, i, sum = 0;
+int i, sum = 0;
 for (i = 0; i < argc; i++)
 ;
 if (i < 3)
@@ -19,7 +19,7 @@ return (0);
 }
 for (i = 1; i < argc; i++)
 {
-if (isdigit(argv[i][j]) && atoi(argv[i]) > 0)
+if (isdigit(*argv[i]) && atoi(argv[i]) > 0)
 {
 sum += atoi(argv[i]);
 }
