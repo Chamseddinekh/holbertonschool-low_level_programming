15ebaec61628a9d982f3c8aebdc8bf1b4801d8e8
Chamseddinekh <chamsseddine.khedhri@gmail.com>
2020-03-03 15:42:35 +0000

    string

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+#include<stdlib.h>
+/**
+ * _strdup - allocate memory array of int
+ *@str: size of arry
+ *Return: pointer to array
+ **/
+char *_strdup(char *str)
+{
+char *p;
+int i, j = 0;
+if (str == NULL)
+return (NULL);
+for (i = 0; str[i] != '\0'; i++)
+;
+p = malloc(i * sizeof(char));
+if (p == NULL)
+return (NULL);
+else
+for (i = 0; str[i] != '\0'; i++)
+{
+p[j] = str[i];
+j++;
+}
+return (p);
+free(p);
+}
