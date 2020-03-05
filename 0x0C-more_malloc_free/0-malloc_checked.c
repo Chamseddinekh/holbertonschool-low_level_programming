a1bd121981dca785f60f98f66b17e7e0486ab304
Chamseddinekh <chamsseddine.khedhri@gmail.com>
2020-03-05 21:15:35 +0000

    alooc

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,20 @@
+#include<stdlib.h>
+#include <limits.h>
+#include "holberton.h"
+/**
+ * malloc_checked - allocate memory 
+ *@b: size of arry
+ *Return: pointer
+ **/
+void *malloc_checked(unsigned int b)
+{
+int *ptr;
+int num = 98;
+ptr = malloc(sizeof(b));
+if (ptr == NULL)
+{
+ptr = &num;
+return (ptr);
+}
+return(ptr);
+}
