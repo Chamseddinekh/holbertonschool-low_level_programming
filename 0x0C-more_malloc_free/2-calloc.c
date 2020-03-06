95c2d6bddb4090b7d82e7cbbfe834f1f54a9af40
Chamseddinekh <chamsseddine.khedhri@gmail.com>
2020-03-06 15:36:23 +0000

    calloc

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,18 @@
+#include<stdlib.h>
+#include "holberton.h"
+/**
+ * string_nconcat - concat 2 strings
+ *@nmemb: number of elemnts
+ *@size: size of bytes
+ *Return: pointer array allocated
+ **/
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+char *arr;
+if (nmemb == 0 || size == 0)
+return (NULL);
+arr = malloc(nmemb * size);
+if (arr == NULL)
+return (NULL);
+return (arr);
+}
