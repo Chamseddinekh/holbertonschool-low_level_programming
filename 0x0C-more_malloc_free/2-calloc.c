fec4eb5efe77c2760375ec4ca456daab9d8c4c7f
Chamseddinekh <chamsseddine.khedhri@gmail.com>
2020-03-06 15:43:18 +0000

    calloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -9,10 +9,13 @@
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 char *arr;
+int i;
 if (nmemb == 0 || size == 0)
 return (NULL);
 arr = malloc(nmemb * size);
 if (arr == NULL)
 return (NULL);
+for(i = 0; arr[i] != '\0'; i++)
+arr[i] = 0;
 return (arr);
 }
