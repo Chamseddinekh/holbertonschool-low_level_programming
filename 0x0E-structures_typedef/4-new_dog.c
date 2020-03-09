f4fd54f0f90001bb7d2e26811e35c339aa31b3c7
Chamseddinekh <chamsseddine.khedhri@gmail.com>
2020-03-09 19:43:28 +0000

    free dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -10,11 +10,14 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 dog_t *black;
+char *cpname, *cpowner;
 black = malloc(sizeof(struct dog));
 if (black == NULL)
 return (NULL);
 black->name = name;
 black->age = age;
 black->owner = owner;
+cpname = (*black).name;
+cpowner = (*black).owner;
 return (black);
 }
---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,11 @@
+#include<stdlib.h>
+#include "dog.h"
+/**
+ *  free_dog - creates a new dog
+ *@d: dog name
+ **/
+void free_dog(dog_t *d);
+{
+if (d != NULL)
+free(d);
+}
