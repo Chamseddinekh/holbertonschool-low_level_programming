211ecd60a1287390f96247e93cb11c7a348066f3
Chamseddinekh <chamsseddine.khedhri@gmail.com>
2020-03-09 15:29:44 +0000

    print name of file executed

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ *main - print name of file executed
+ *@argc: number of arguments
+ *@argv: table of arguments
+ *Return: 0
+ **/
+int main()
+{
+printf("%s\n", __FILE__);
+return (0);
+}
