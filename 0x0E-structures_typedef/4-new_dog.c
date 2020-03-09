978b2408bdf99f0c48c6ff1e697413d04a9c0dae
Chamseddinekh <chamsseddine.khedhri@gmail.com>
2020-03-09 18:59:09 +0000

    create new dg

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,20 @@
+#include<stdlib.h>
+#include "dog.h"
+/**
+ * new_dog - creates a new dog
+ *@name: dog name
+ *@age: dog age
+ *@owner: dog owner
+ *Return: pointer to new dog
+ **/
+dog_t *new_dog(char *name, float age, char *owner)
+{
+dog_t *black;
+black = malloc(sizeof(struct dog));
+if (black == NULL)
+return (NULL);
+black->name = name;
+black->age = age;
+black->owner = owner;
+return (black);
+}
