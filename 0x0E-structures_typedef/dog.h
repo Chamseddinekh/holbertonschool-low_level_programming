cbd770c34d251b0e85afbfe546497680b95a5f34
Chamseddinekh <chamsseddine.khedhri@gmail.com>
2020-03-09 16:50:19 +0000

    def struct dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,11 +2,11 @@
  * struct dog - dog and owner info
  *@name: name of dog
  *@age: age of dog
- *@owner: owner of dog  
+ *@owner: owner of dog
  */
 struct dog
 {
 char *name;
 float age;
 char *owner;
-}my_dog;
+} my_dog;
