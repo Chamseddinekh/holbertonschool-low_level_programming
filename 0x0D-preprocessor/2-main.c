f1388ddf765eee341d49d21db533b4bb5ade0da4
Chamseddinekh <chamsseddine.khedhri@gmail.com>
2020-03-09 15:31:15 +0000

    fixed print name of file executed

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,11 +1,9 @@
 #include <stdio.h>
 /**
  *main - print name of file executed
- *@argc: number of arguments
- *@argv: table of arguments
  *Return: 0
  **/
-int main()
+int main(void)
 {
 printf("%s\n", __FILE__);
 return (0);
