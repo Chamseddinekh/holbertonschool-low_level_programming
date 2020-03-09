3a2273846ff501855f94d76dc4a605a54bd3bd7e
Chamseddinekh <chamsseddine.khedhri@gmail.com>
2020-03-09 17:24:09 +0000

    intialise dog info

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,15 @@
+#include<stdlib.h>
+#include "dog.h"
+/**
+ * init_dog - dog information
+ *@name: dog name
+ *@age: dog age
+ *@owner: dog owner
+ *@d: dog structure
+ **/
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+d->name = name;
+d->age = age;
+d->owner = owner;
+}
