0d32976c06d483823352eae129a59e48ca48946d
Chamseddinekh <chamsseddine.khedhri@gmail.com>
2020-03-09 16:59:08 +0000

    fixed header

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,12 +1,16 @@
+#ifndef DOG_H
+#define DOG_H
 /**
  * struct dog - dog and owner info
  *@name: name of dog
  *@age: age of dog
  *@owner: owner of dog
- */
+ *Description: information about the dog and his owner
+*/
 typedef struct dog
 {
 char *name;
 float age;
 char *owner;
 } my_dog;
+#endif
