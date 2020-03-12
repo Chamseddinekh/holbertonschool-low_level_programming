3a89e6a73e5249934795b8df523f8bde46fc0099
Chamseddinekh <chamsseddine.khedhri@gmail.com>
2020-03-12 17:49:55 +0100

    print fixed

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,5 +1,10 @@
 #include "function_pointers.h"
 #include <stdio.h>
+/**
+* print_name - print name
+*@name: name to print
+*@f: function print
+*/
 void print_name(char *name, void (*f)(char *))
 {
 if (name != NULL && f != NULL)
