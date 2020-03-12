b42a35f63221b7492eaf019ed70e29e44fb90826
Chamseddinekh <chamsseddine.khedhri@gmail.com>
2020-03-12 19:12:14 +0100

    print array

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,17 @@
+#include "function_pointers.h"
+/**
+* array_iterator - print array
+*@array: pointer to array
+*@size: size of array
+@action: function pointer
+*/
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+unsigned int i;
+if (array != NULL && action != NULL)
+{
+for (i = 0; i < size; i++)
+action(array[i]);
+}
+return;
+}
