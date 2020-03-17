278548c6d3265755279954a47a93b54adce1aec2
Chamseddinekh <chamsseddine.khedhri@gmail.com>
2020-03-17 22:30:34 +0100

    sum of arg

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdarg.h>
+/**
+* sum_them_all - sum of n integer
+*@n: number of interger passed
+*Return: return sum of all argumets
+*/
+int sum_them_all(const unsigned int n, ...)
+{
+int i, sum = 0;
+va_list PtrN;
+va_start(PtrN, n);
+for (i = 0; i < n; i++)
+sum += va_arg(PtrN, int);
+va_end(PtrN);
+return (sum);
+}
