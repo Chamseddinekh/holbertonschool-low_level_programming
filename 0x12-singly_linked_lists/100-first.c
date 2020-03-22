0a110590f3900a9f435a4f70246d0702b913caa0
Chamseddinekh <chamsseddine.khedhri@gmail.com>
2020-03-22 23:00:35 +0100

    print before main func

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+/**
+*add_node - funtion add node
+*@head: Structure of linked list
+*@str: string to add in the node
+*Return: the address of the new element, or NULL if it failed
+*/
+void PrintBeforeMain()
+{
+    printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
