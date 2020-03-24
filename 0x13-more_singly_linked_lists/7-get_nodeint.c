220034c467b7d9168bdcecd43123449a5ec093b5
Chamseddinekh <chamsseddine.khedhri@gmail.com>
2020-03-24 22:39:40 +0100

    get index

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -8,13 +8,21 @@
 
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
-unsigned   int i;
-while (head)
+unsigned   int i, j = 0;
+listint_t *cmp = head;
+
+while (cmp != NULL)
+{
+cmp = cmp->next;
+j++;
+}
+if (index <= j)
 {
 for (i = 0; i <= index; i++)
 {
 if (i == index)
 return (head);
+
 head = head->next;
 }
 }
