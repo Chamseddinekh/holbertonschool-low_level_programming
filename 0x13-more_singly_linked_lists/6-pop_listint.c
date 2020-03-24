448759d0d7f89eb845d3a088ca909522ce932020
Chamseddinekh <chamsseddine.khedhri@gmail.com>
2020-03-24 21:54:59 +0100

    pop list

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * pop_listint - pop list
+ * @head: list to print
+ */
+int pop_listint(listint_t **head)
+{
+    listint_t *ptr = *head;
+    int n;
+if (*head == NULL)
+return (0);
+
+n = ptr->n;
+free(ptr);
+*head = ptr->next;
+return (n);
+}
