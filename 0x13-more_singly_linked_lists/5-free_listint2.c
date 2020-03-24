ca2ae107fa148e81a4e96802d889f9b044835c2c
Chamseddinekh <chamsseddine.khedhri@gmail.com>
2020-03-24 20:20:28 +0100

    free fixed

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -5,15 +5,14 @@
  */
 void free_listint2(listint_t **head)
 {
-listint_t *ptr = *head;
+listint_t *ptr;
 
-while (ptr)
+while (*head != NULL)
 {
+ptr = *head;
+*head = (*head)->next;
 free(ptr);
-*head = NULL;
-ptr = ptr->next;
 }
-
-
+head = NULL;
 
 }
