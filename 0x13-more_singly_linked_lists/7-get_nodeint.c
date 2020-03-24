de526f444c790a2a99b12aa6d5327ec61e3999b4
Chamseddinekh <chamsseddine.khedhri@gmail.com>
2020-03-24 22:25:35 +0100

    get index

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -9,12 +9,14 @@
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 unsigned   int i;
-
+while (head)
+{
 for (i = 0; i <= index; i++)
 {
 if (i == index)
 return (head);
 head = head->next;
+}
 }
 return (NULL);
 }
