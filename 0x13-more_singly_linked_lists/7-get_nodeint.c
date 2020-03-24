f33006d8e3cc6f52a5068b6da4cfd9013ae6ec92
Chamseddinekh <chamsseddine.khedhri@gmail.com>
2020-03-24 22:24:17 +0100

    get index

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+ * get_nodeint_at_index - pop list
+ * @head: list to print
+ * @index: index to print
+ * Return: return node content
+ */
+
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+unsigned   int i;
+
+for (i = 0; i <= index; i++)
+{
+if (i == index)
+return (head);
+head = head->next;
+}
+return (NULL);
+}
