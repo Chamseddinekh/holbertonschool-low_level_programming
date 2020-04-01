af48d42d5cd73fad2678cee20e610a559ad2d4c1
Chamseddinekh <chamsseddine.khedhri@gmail.com>
2020-04-01 06:15:34 +0100

    h

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -11,35 +11,35 @@ int off, oft, rff, wft, cl, cl1;
 char buffer[blok];
 if (ac != 3)
 {
-dprintf(STDERR_FILENO,"Usage: cp %s %s\n", av[1], av[2]);
+dprintf(STDERR_FILENO, "Usage: cp %s %s\n", av[1], av[2]);
 exit(97);
 }
-off = open(av[1], O_RDWR, 0641);
+off = open(av[1], O_RDONLY);
 if (off == -1)
 {
-dprintf(STDERR_FILENO,"Error: Can't read from %s\n", av[1]);
+dprintf(STDERR_FILENO, "Error: Can't read from %s\n", av[1]);
 exit(98);
 }
-oft = open(av[2], O_CREAT | O_RDWR | O_TRUNC, 0641);
+oft = open(av[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 if (off == -1)
 exit(99);
 rff = read(off, buffer, 1024);
 if (rff == -1)
 {
-dprintf(STDERR_FILENO,"Error: Can't read from file  %s\n", av[1]);
+dprintf(STDERR_FILENO, "Error: Can't read from file  %s\n", av[1]);
 exit(98);
 }
 wft = write(oft, buffer, rff);
 if (wft == -1)
 {
-dprintf(STDERR_FILENO,"Error: Can't write to  %s\n", av[2]);
+dprintf(STDERR_FILENO, "Error: Can't write to  %s\n", av[2]);
 exit(99);
 }
 cl = close(off);
 cl1 = close(oft);
 if (cl == -1 || cl1 == -1)
 {
-dprintf(STDERR_FILENO,"Error: Can't close fd %d\n", off);
+dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", off);
 return (100);
 }
 return (0);
