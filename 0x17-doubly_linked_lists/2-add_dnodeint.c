9808d07f98a7302cb096b898fc68ca789c47e211
Chamseddinekh <chamsseddine.khedhri@gmail.com>
2020-05-03 17:39:17 +0100

    add node

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -11,12 +11,13 @@ dlistint_t *new;
 new = (dlistint_t *)malloc(sizeof(dlistint_t));
 if (new == NULL)
 return (NULL);
+
 new->n = n;
 new->prev = NULL;
 new->next = *head;
 
 (*head)->prev = new;
-*head = new;
+
 
 return (new);
 }
