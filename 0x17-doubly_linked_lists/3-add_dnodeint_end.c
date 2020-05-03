4757af8f0e77a1be68c4a5dbd85d11fd491edb53
Chamseddinekh <chamsseddine.khedhri@gmail.com>
2020-05-03 18:01:07 +0100

    add end fixed

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -1,6 +1,6 @@
 #include "lists.h"
 /**
- * add_nodeint_end - adds a new node at the end
+ * add_dnodeint_end - adds a new node at the end
  * @head: list to print
  * @n: element of the list
  * Return: the address of the new element
@@ -11,16 +11,22 @@ dlistint_t *tmp, *ptr;
 tmp = malloc(sizeof(dlistint_t));
 if (tmp == NULL)
 return (NULL);
+
 tmp->n = n;
 tmp->next = NULL;
 if (*head == NULL)
+{
+tmp->prev = NULL;
 *head = tmp;
+}
 else
 {
 ptr = *head;
 while (ptr->next)
 ptr = ptr->next;
 ptr->next = tmp;
+tmp->prev = ptr;
 }
+
 return (ptr);
 }
