9cb1a4bba1b245f388551a033ac4e637fc0294d7
Chamseddinekh <chamsseddine.khedhri@gmail.com>
2020-05-03 18:46:24 +0100

    fixed

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -5,39 +5,35 @@
 
 /**
  *insert_dnodeint_at_index - function
- *@head: list
+ *@h: list
  *@idx: int
  *@n: int
  *Return: list
  */
-dlistint_t *insert_dnodeint_at_index(dlistint_t **head, unsigned int idx, int n)
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	unsigned int i;
-	dlistint_t *node = *head;
-	dlistint_t *new = NULL;
-
-	new = malloc(sizeof(dlistint_t));
-	if (new == NULL)
-		return (NULL);
-	new->n = n;
-	new->next = NULL;
-
-	if (idx == 0)
-	{
-		new->next = *head;
-        new->prev = NULL;
-		*head = new;
-		return (new);
-	}
-
-	for (i = 0; i < idx - 1; i++)
-	{
-		if (!node->next)
-			return (NULL);
-		node = node->next;
-	}
-	new->next = node->next;
-	node->next = new;
-
-	return (new);
+unsigned int i;
+dlistint_t *node = *h;
+dlistint_t *new = NULL;
+new = malloc(sizeof(dlistint_t));
+if (new == NULL)
+return (NULL);
+new->n = n;
+new->next = NULL;
+if (idx == 0)
+{
+new->next = *h;
+new->prev = NULL;
+*h = new;
+return (new);
+}
+for (i = 0; i < idx - 1; i++)
+{
+if (!node->next)
+return (NULL);
+node = node->next;
+}
+new->next = node->next;
+node->next = new;
+return (new);
 }
